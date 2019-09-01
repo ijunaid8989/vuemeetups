e146536dd15dbee3fcbef949e5f13bf9f0d263ed
Junaid Farooq <junaid@evercam.io>
2019-09-01 21:20:03 +0500

    Refactored Toolbar.

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,10 @@
       <v-list dense>
         <v-list-item @click="">
           <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
+            <v-icon>fa fa-users</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item @click="">
-          <v-list-item-action>
-            <v-icon>mdi-contact-mail</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
+            <v-list-item-title>View Meetups</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -28,10 +20,10 @@
       app
       dense
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-app-bar-nav-icon>
       <v-toolbar-title>DevMeetUp</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items class="hidden-xs-only">
         <v-btn text>
           <v-icon>fa fa-users</v-icon> View Meetup
         </v-btn>
@@ -45,22 +37,6 @@
       >
       </v-container>
     </v-content>
-<!--     <v-card
-      color="grey lighten-4"
-      flat
-      tile
-    >
-      <v-toolbar dense>
-        <v-toolbar-title>DevMeetup</v-toolbar-title>
-
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn flat>
-            <v-icon>fa fa-users</v-icon> View Meetup
-          </v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-    </v-card> -->
     <main></main>
   </v-app>
 </template>
