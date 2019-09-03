f760f449cde42ffc8d26522a44df186e413de12a
Junaid Farooq <junaid@evercam.io>
2019-09-03 08:14:52 +0500

    Basic Setup for layout.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <v-app>
     <v-navigation-drawer
+      temporary
       v-model="drawer"
       app
     >
       <v-list dense>
-        <v-list-item @click="" v-for="item in menuItems" :key="item.title">
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.title"
+          router
+          :to="item.link"
+          >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -21,10 +27,18 @@
       dense
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-app-bar-nav-icon>
-      <v-toolbar-title>DevMeetUp</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">DevMeetUp</router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn text v-for="item in menuItems" :key="item.title">
+        <v-btn
+          text
+          v-for="item in menuItems"
+          :key="item.title"
+          router
+          :to="item.link"
+          >
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
         </v-btn>
@@ -33,14 +47,14 @@
 
     <v-content>
       <v-container
-        class="fill-height"
+        
         fluid
       >
+      <main>
+        <router-view></router-view>
+      </main>
       </v-container>
     </v-content>
-    <main>
-      <router-view></router-view>
-    </main>
   </v-app>
 </template>
 
@@ -53,11 +67,11 @@ export default {
   data: () => ({
     drawer: false,
     menuItems: [
-      {icon: "fa fa-users", title: "View Meetups"},
-      {icon: "fa fa-map-marker", title: "Organize Meetup"},
-      {icon: "fa fa-user-circle", title: "Profile"},
-      {icon: "fa fa-key", title: "Sign In"},
-      {icon: "fa fa-plug", title: "Sign Up"}
+      {icon: "fa fa-users", title: "View Meetups", link: "/meetups"},
+      {icon: "fa fa-map-marker", title: "Organize Meetup", link: "/meetup/new"},
+      {icon: "fa fa-user-circle", title: "Profile", link: "/profile"},
+      {icon: "fa fa-key", title: "Sign In", link: "/signin"},
+      {icon: "fa fa-plug", title: "Sign Up", link: "/signup"}
     ]
   }),
 };
