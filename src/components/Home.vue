934dcfd20a08e842c873fbd24c8b0ca490357bd1
Junaid Farooq <junaid@evercam.io>
2019-09-05 12:35:59 +0500

    Added carousel.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,5 +8,31 @@
         <v-btn large router to="/meetup/new" class="info">Organize Meetups</v-btn>
       </v-flex>
     </v-layout>
+    <v-layout row wrap mt-5>
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :src="meetup.imageUrl"
+            :key="meetup.id"
+          ></v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        meetups: [
+          { imageUrl: "https://blog.hotelscombined.com/wp-content/uploads/2016/12/New-York-Times-Square.jpg", id: "dsgsgsd335", title: "Meetup in New York"},
+          {
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQue7DMZ_lbT8lV8a23Lx6JZ9S71uix3W07SWfE1pFPe-bAl-EZ", id: "dfdsgdsg", title: "Meetup in New York"
+          }
+        ]
+      }
+    }
+  }
+</script>
