0cae8a399e2e86af27cec4a5993dcd8e6394363f
Junaid Farooq <junaid@evercam.io>
2019-09-06 15:42:14 +0500

    Added Meetup.

---
+++ src/components/Meetup/Meetup.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-card-title>
+            <h6 class="info--text">My Meetup</h6>
+          </v-card-title>
+          <v-img
+            src="https://blog.hotelscombined.com/wp-content/uploads/2016/12/New-York-Times-Square.jpg"
+            height="130px">
+          </v-img>
+          <v-card-text>
+            <div class="info--text">17 July 017 where it took place</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="info">Register</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
