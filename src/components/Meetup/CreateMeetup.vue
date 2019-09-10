4d69c992a7f22bcbce99dc58df70e810f7edc21e
Junaid Farooq <junaid@evercam.io>
2019-09-10 20:43:39 +0500

    Added Create Meetup form.

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,101 @@
 <template>
-  <div>
-    <p>The create meet up page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h4 class="primary--text">Create a new Meetup</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <form>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="title"
+                label="Title"
+                id="title"
+                v-model="title"
+                :rules="[rules.required]"
+              >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="location"
+                label="Location"
+                id="location"
+                v-model="location"
+                :rules="[rules.required]"
+              >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="imageUrl"
+                label="Image URL"
+                id="image-url"
+                v-model="imageUrl"
+                :rules="[rules.required]"
+              >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl" alt="Meetup Image">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-textarea
+                name="description"
+                label="Description"
+                id="descirption"
+                multiline
+                v-model="description"
+                :rules="[rules.required]"
+              >
+              </v-textarea>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn :disabled="!formIsValid" class="primary" :data="formIsValid">
+                Create Meetup
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        title: "",
+        description: "",
+        imageUrl: "",
+        location: "",
+        rules: {
+          required: value => !!value || 'Required.'
+        }
+      }
+    },
+    computed: {
+      formIsValid () {
+        return 
+          this.title !== '' &&
+          this.location !== '' &&
+          this.imageUrl !== '' &&
+          this.description !== ''
+      }
+    }
+  }
+</script>
